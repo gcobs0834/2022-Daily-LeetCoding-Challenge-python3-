d96f957585086b764271d9ddbec393dfb9663003
YiHsin Chen <gcobs0834@gmail.com>
2022-01-24 11:12:17 +0800

    Update Solution.py

--- 1291. Sequential Digits (Medium)/Solution.py
+++ 1291. Sequential Digits (Medium)/Solution.py
@@ -21,16 +21,15 @@ class Solution1:
         return sorted(res)
 
 # O(n) Better
-class Solution2:
+class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        sequentialDigit = '123456789'
-        res = []
+        sequential = "123456789"
         lowerBound = len(str(low))
-        upperBound = len(str(high)) + 1
-        
-        for length in range(lowerBound, upperBound):
-            for idx in range(10 - length):
-                num = int(sequentialDigit[idx: idx + length])
-                if low <= num <= high:
-                    res.append(num)
+        upperBound = len(str(high))
+        res = []
+        for length in range(lowerBound, upperBound + 1):
+            for i in range(0, 10 - length):
+                currentNum = int(sequential[i: i+length])
+                if low <= currentNum <= high:
+                    res.append(currentNum)
         return res
